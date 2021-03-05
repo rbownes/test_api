26254c4d246c96eeabdc52e2b8a5bf47a21068cf
rbownes <richard.bownes@bbc.co.uk>
2021-03-05 13:02:58 +0000

    testing web api

---
+++ server.py
@@ -0,0 +1,27 @@
+from src.generator import add_problem
+import random
+from typing import Optional
+from fastapi import FastAPI, Request
+
+
+app = FastAPI()
+
+question = add_problem(1)
+operators = {
+    "Addition": "+",
+    "Subtraction": "-",
+    "Multiplication": "*",
+    "Division": "/",
+}
+
+
+@app.get("/{arithmatic}")
+def read_root(arithmatic: str):
+    if arithmatic == "Random":
+        return question.create_random_problem()
+    elif arithmatic == "Operator":
+        sign = "Operator"
+        return question.create_arithmatic_problem(sign)
+    else:
+        sign = operators.get(arithmatic)
+        return question.create_arithmatic_problem(sign)
